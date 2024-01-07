fd9720c9931dbd58e923aa904823c6fbcba9d773
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2024-01-07 02:43:17 +0200

    додав переклад в Sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,11 +1,14 @@
 import "./Sidebar.scss";
 
+import { useTranslations } from "next-intl";
+
 import Link from "next/link";
 
 export default function Sidebar() {
+  const t = useTranslations("Sidebar");
   return (
     <div className="sidebar rounded-b-xl h-[fit-content]">
-      <h3 className="sidebar-title">Categories:</h3>
+      <h3 className="sidebar-title">{t("categories")}:</h3>
       <ul className="sidebar-category  gap-6 list-none  ">
         <li>
           <Link href="/iphone">iPhone</Link>
